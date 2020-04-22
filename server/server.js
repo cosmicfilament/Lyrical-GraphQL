91a8812d58cafb6b740371b05c4d3ac782016a37
cosmicfilament <john@thecosmicfilament.com>
2020-04-22 18:11:40 -0400

    redid front_end using create_react_app and newer apollo client

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const models = require('./models');
 const expressGraphQL = require('express-graphql');
 const mongoose = require('mongoose');
@@ -20,6 +21,8 @@ mongoose.connection
 	.once('open', () => console.log('Connected to mongodb instance.'))
 	.on('error', error => console.log('Error connecting to mongodb:', error));
 
+app.use(cors());
+
 app.use(bodyParser.json());
 app.use(
 	'/graphql',
@@ -29,9 +32,9 @@ app.use(
 	})
 );
 
-const webpackMiddleware = require('webpack-dev-middleware');
-const webpack = require('webpack');
-const webpackConfig = require('../webpack.config.js');
-app.use(webpackMiddleware(webpack(webpackConfig)));
+// const webpackMiddleware = require('webpack-dev-middleware');
+// const webpack = require('webpack');
+// const webpackConfig = require('../webpack.config.js');
+// app.use(webpackMiddleware(webpack(webpackConfig)));
 
 module.exports = app;
